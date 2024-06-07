0b7c97cb700aa62bf739826fe0ae75d4a5d10586
Venkateshnirati <130686136+Venkateshneerati@users.noreply.github.com>
2024-06-07 21:04:12 +0530

    Update facerecognition.java

--- facerecognition.java
+++ facerecognition.java
@@ -20,15 +20,11 @@ class  DetectFaceDemo {
         CascadeClassifier faceDetector = new CascadeClassifier();
         faceDetector.load( "C:\\Users\\Desktop\\ocv\\.idea\\haarcascade_frontalface_alt.xml" );
 
-
-
         com.ocr.ocrexample.FileUploadController.convert(file);
                 Mat image = Imgcodecs.imread(String.valueOf(file));
 
         MatOfRect faceDetections = new MatOfRect();
         faceDetector.detectMultiScale( image, faceDetections );
-
-        // Creating a rectangular box showing faces detected
         for (Rect rect : faceDetections.toArray()) {
             rectangle( image, new Point( rect.x, rect.y ), new Point( rect.width + rect.x,
                     rect.height + rect.y ), new Scalar( 0, 255, 0 ) );
